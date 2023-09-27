3821671b98d44c1c85eabcf3f4d79f0db0d925af
Sergey Armodin <dolph-in@yandex.ru>
2023-09-27 12:37:50 +0300

    fixed typo in TRY currency value

--- ModulbankInformer/Models/BankAccount.swift
+++ ModulbankInformer/Models/BankAccount.swift
@@ -29,7 +29,7 @@ struct BankAccount: Codable, Identifiable {
         case byn = "BYN"
         case tjs = "TJS"
         case uzs = "UZS"
-        case `try` = "TYR"
+        case `try` = "TRY"
         case kgs = "KGS"
 
         var imageName: String {
