da3f3816d50e319f6c776ccecb96aa2838f4face
Sergey Armodin <dolph-in@yandex.ru>
2024-04-03 12:01:31 +0300

    spaces

--- ModulbankInformer/APIStore.swift
+++ ModulbankInformer/APIStore.swift
@@ -13,83 +13,83 @@ private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: 
 // API docs: https://api.modulbank.ru
 
 final class APIStore: ObservableObject {
-    private let keyStore = KeyStore()
+	private let keyStore = KeyStore()
 
-    @Published var hasAPIKey: Bool
-    @Published var accounts: [AccountInfo] = []
+	@Published var hasAPIKey: Bool
+	@Published var accounts: [AccountInfo] = []
 
-    enum APIError: Error {
-        case badURL
-    }
+	enum APIError: Error {
+		case badURL
+	}
 
-    enum HTTPMethod: String {
-        case GET, POST, PUT, DELETE
-    }
+	enum HTTPMethod: String {
+		case GET, POST, PUT, DELETE
+	}
 
-    var timer: Timer?
+	var timer: Timer?
 
-    init(accounts: [AccountInfo] = []) {
-        self.accounts = accounts
-        self.hasAPIKey = keyStore.getAPIKey() != nil
-        self.timer = Timer.scheduledTimer(withTimeInterval: 60*5, repeats: true, block: { timer in
-            Task {
+	init(accounts: [AccountInfo] = []) {
+		self.accounts = accounts
+		self.hasAPIKey = keyStore.getAPIKey() != nil
+		self.timer = Timer.scheduledTimer(withTimeInterval: 60*5, repeats: true, block: { timer in
+			Task {
 				do {
 					try await self.getAccountInfo()
 				} catch {
 					logger.error("\(String(describing: error), privacy: .public)")
 				}
-            }
-        })
-    }
+			}
+		})
+	}
 
-    func setAPIKey(_ key: String?) {
-        self.hasAPIKey = key != nil
+	func setAPIKey(_ key: String?) {
+		self.hasAPIKey = key != nil
 
-        if key == nil {
-            Task {
-                self.keyStore.saveAPIKey(key)
-            }
-            self.accounts = []
-        } else {
-            Task {
-                self.keyStore.saveAPIKey(key)
-            }
-        }
-    }
+		if key == nil {
+			Task {
+				self.keyStore.saveAPIKey(key)
+			}
+			self.accounts = []
+		} else {
+			Task {
+				self.keyStore.saveAPIKey(key)
+			}
+		}
+	}
 
-    func getAccountInfo() async throws {
-        guard keyStore.getAPIKey() != nil else {
-            setAPIKey(nil)
-            return
-        }
+	func getAccountInfo() async throws {
+		guard keyStore.getAPIKey() != nil else {
+			setAPIKey(nil)
+			return
+		}
 
-        let decoder = JSONDecoder()
+		let decoder = JSONDecoder()
 
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "api.modulbank.ru"
-        components.path = "/v1/account-info"
+		var components = URLComponents()
+		components.scheme = "https"
+		components.host = "api.modulbank.ru"
+		components.path = "/v1/account-info"
 
-        guard let url = components.url else {
-            throw APIError.badURL
-        }
+		guard let url = components.url else {
+			throw APIError.badURL
+		}
 
 		logger.info("Getting data")
-        let request = makeRequest(withMethod: .POST, fromURL: url)
-        let (data, _) = try await URLSession.shared.data(for: request)
-        let response = try decoder.decode([AccountInfo].self, from: data)
+		let request = makeRequest(withMethod: .POST, fromURL: url)
+		let (data, _) = try await URLSession.shared.data(for: request)
+		let response = try decoder.decode([AccountInfo].self, from: data)
 		await MainActor.run {
 			self.accounts = response
 		}
-    }
-
-    private func makeRequest(withMethod method: HTTPMethod, fromURL url: URL) -> URLRequest {
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
-
-        if let apiKey = keyStore.getAPIKey() {
-            request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-        }
-        return request
-    }
+	}
+	
+	private func makeRequest(withMethod method: HTTPMethod, fromURL url: URL) -> URLRequest {
+		var request = URLRequest(url: url)
+		request.httpMethod = method.rawValue
+		
+		if let apiKey = keyStore.getAPIKey() {
+			request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+		}
+		return request
+	}
 }
